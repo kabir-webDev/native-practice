3c6f0c2c3d640a452a283f8b3eaf7f2b80cfe647
kabir-webDev <kabir.webdev@gmail.com>
2020-09-04 22:45:29 +0600

    More text box

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={styles.text}>This will test up the whole application. </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "red",
+    backgroundColor: "green",
+  },
+  auto: {
+    borderRadius: "1px solid gray",
   },
 });

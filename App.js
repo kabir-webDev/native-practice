217f3be163cedcbf8a28ff95e680c0286211f7b0
kabir-webDev <kabir.webdev@gmail.com>
2020-09-05 16:57:49 +0600

    Adding text field

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button, TextInput } from "react-native";
+import { View, Button, TextInput, Text } from "react-native";
 
 class App extends React.Component {
   constructor() {
@@ -15,28 +15,43 @@ class App extends React.Component {
   }
   render() {
     return (
-      <View style={{ margin: 20, marginTop: 100, backgroundColor: "teal" }}>
+      <View style={{ margin: 20, marginTop: 100 }}>
         <TextInput
           placeholder="enter your name"
           onChangeText={(text) => {
             this.setState({ name: text });
           }}
-          style={{ borderWidth: 2, borderColor: "skyblue", margin: 20 }}
+          style={{
+            borderWidth: 2,
+            borderColor: "skyblue",
+            padding: 10,
+            margin: 20,
+          }}
         />
         <TextInput
           placeholder="enter your email"
           onChangeText={(text) => {
             this.setState({ email: text });
           }}
-          style={{ borderWidth: 2, borderColor: "skyblue", margin: 20 }}
+          style={{
+            borderWidth: 2,
+            borderColor: "skyblue",
+            padding: 10,
+            margin: 20,
+          }}
         />
         <TextInput
-          placeholder="enter your password"
+          placeholder="enter your passWord"
           secureTextEntry={true}
           onChangeText={(text) => {
             this.setState({ password: text });
           }}
-          style={{ borderWidth: 2, borderColor: "skyblue", margin: 20 }}
+          style={{
+            borderWidth: 2,
+            borderColor: "skyblue",
+            padding: 10,
+            margin: 20,
+          }}
         />
         <Button
           title="Submit"
@@ -44,6 +59,11 @@ class App extends React.Component {
             this.submit();
           }}
         />
+        <Text>
+          <Text>{this.state.name}</Text> ||
+          <Text>{this.state.email}</Text> ||
+          <Text>{this.state.password}</Text> ||
+        </Text>
       </View>
     );
   }

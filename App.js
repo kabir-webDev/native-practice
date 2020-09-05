07e4f9aa58cd3b137ca66a7d23ef501ab4b27d40
kabir-webDev <kabir.webdev@gmail.com>
2020-09-05 12:05:30 +0600

    Register form

--- App.js
+++ App.js
@@ -1,31 +1,52 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Button, TextInput } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Text style={styles.text}>This will test up the whole application. </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+  submit() {
+    console.warn(this.state);
+  }
+  render() {
+    return (
+      <View style={{ margin: 20, marginTop: 100, backgroundColor: "teal" }}>
+        <TextInput
+          placeholder="enter your name"
+          onChangeText={(text) => {
+            this.setState({ name: text });
+          }}
+          style={{ borderWidth: 2, borderColor: "skyblue", margin: 20 }}
+        />
+        <TextInput
+          placeholder="enter your email"
+          onChangeText={(text) => {
+            this.setState({ email: text });
+          }}
+          style={{ borderWidth: 2, borderColor: "skyblue", margin: 20 }}
+        />
+        <TextInput
+          placeholder="enter your password"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            this.setState({ password: text });
+          }}
+          style={{ borderWidth: 2, borderColor: "skyblue", margin: 20 }}
+        />
+        <Button
+          title="Submit"
+          onPress={() => {
+            this.submit();
+          }}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "red",
-    backgroundColor: "green",
-  },
-  auto: {
-    borderRadius: "1px solid gray",
-  },
-});
+export default App;
